0aed1c23e18b4efc02c89c392641ceaf50de23cc
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-10 12:52:37 +0300

    weather fetch works now

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,44 @@
 const Weather = () => {
-  <h1>Hello</h1>;
+  const APIkey = "add88e3395b3389388ec8f68dad58c25";
+  const coordinates = {
+    latitude: "10.99",
+    longitude: "44.34",
+  };
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    coordinates.latitude +
+    "&lon=" +
+    coordinates.longitude +
+    "&appid=" +
+    APIkey +
+    "&units=" +
+    unit;
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        // console.log(data); // didnt delete it in case any need
+        const generalInfo = data.main;
+        const weatherInfo = data.weather;
+        const windInfo = data.wind;
+
+        console.table(generalInfo);
+        console.table(weatherInfo);
+        console.table(windInfo);
+      } else {
+        console.error("error");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //   fetchWeatherData();
+
+  return <h1>Hello</h1>;
 };
 
 export default Weather;
